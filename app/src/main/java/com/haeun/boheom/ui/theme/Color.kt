6dfbf0096a38c3ac87f0b9b21d3754388a27855f
chlgkdms <haeun3882@gmail.com>
2024-01-10 20:14:12 +0900

    setting :: ui color setting

--- app/src/main/java/com/haeun/boheom/ui/theme/Color.kt
+++ app/src/main/java/com/haeun/boheom/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package com.haeun.boheom.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Main = Color(0xFF44EA51)
+val LightGreen = Color(0xFF87EA2C)
+
+val Gray500 = Color(0xFF828282)
+val Gray400 = Color(0xFF9B9B9B)
+val Gray300 = Color(0xFFB4B4B4)
+val Gray200 = Color(0xFFCDCDCD)
+val Gray100 = Color(0xFFE6E6E6)
+val Gray50 = Color(0xFFF3F3F3)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
