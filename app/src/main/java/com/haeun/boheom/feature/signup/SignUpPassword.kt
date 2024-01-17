37bfd343bca92e7e8794c8eb638e22e1e897aa6c
chlgkdms <haeun3882@gmail.com>
2024-01-17 18:29:33 +0900

    add :: signUp 비밀번호

---
+++ app/src/main/java/com/haeun/boheom/feature/signup/SignUpPassword.kt
@@ -0,0 +1,10 @@
+package com.haeun.boheom.feature.signup
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun SignUpPassword() {
+
+}
