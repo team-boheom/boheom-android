0a0e640b18b88600b9b6ffb65e75b351e8ad5085
chlgkdms <haeun3882@gmail.com>
2024-01-17 18:45:11 +0900

    add :: signUp password 뷰

--- app/src/main/java/com/haeun/boheom/feature/signup/SignUpPassword.kt
+++ app/src/main/java/com/haeun/boheom/feature/signup/SignUpPassword.kt
@@ -1,10 +1,47 @@
 package com.haeun.boheom.feature.signup
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.haeun.boheom.R
+import com.haeun.boheom.ui.theme.Black
+import com.haeun.boheom.ui.theme.Gray600
+import com.haeun.boheom.ui.theme.pretendardFamily
 
 @Composable
 internal fun SignUpPassword() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 16.dp),
+    ) {
+        Text(
+            modifier = Modifier.padding(top = 70.dp),
+            text = stringResource(id = R.string.what_are_you_password),
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = Black,
+        )
+        Text(
+            text = stringResource(id = R.string.what_are_you_password_description),
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Light,
+            fontSize = 16.sp,
+            color = Gray600,
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+    }
 }
