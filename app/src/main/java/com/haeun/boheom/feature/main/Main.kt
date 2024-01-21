785ac211a5ec78450621d6bb9dc8de11b598daf7
chlgkdms <haeun3882@gmail.com>
2024-01-21 23:20:23 +0900

    add :: main file

--- app/src/main/java/com/haeun/boheom/feature/main/Main.kt
+++ app/src/main/java/com/haeun/boheom/feature/main/Main.kt
@@ -1,8 +1,15 @@
 package com.haeun.boheom.feature.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 internal fun Main() {
-    
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        
+    }
 }
