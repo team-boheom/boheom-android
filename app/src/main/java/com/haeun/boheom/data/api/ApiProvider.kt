a54352ecbc275c0bf59c1426c5cf977e5301c72f
chlgkdms <haeun3882@gmail.com>
2024-01-31 17:47:11 +0900

    add :: ApiProvider 추가

---
+++ app/src/main/java/com/haeun/boheom/data/api/ApiProvider.kt
@@ -0,0 +1,78 @@
+package com.haeun.boheom.data.api
+
+import android.content.Context
+import android.content.SharedPreferences
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiProvider {
+    private var BASE_URL = "https://3.36.60.11:8080/"
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    /**
+     * this function save token with shared Preference
+     * @param context
+     */
+    fun initialize(context: Context) {
+        sharedPreferences = context.getSharedPreferences("my_shared_prefs", Context.MODE_PRIVATE)
+    }
+
+
+    /**
+     * this function show http logs
+     */
+    private fun getLoggingInterceptor() =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+
+    /**
+     * Returns a configured Retrofit instance for network requests with logging, token authentication,
+     * and Gson serialization/deserialization
+     * @return Configured Retrofit instance
+     */
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(getLoggingInterceptor())
+                    .addInterceptor(getTokenInterceptor())
+                    .build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    /**
+     * this function puts a token in the header
+     * @return interceptor that include header token
+     */
+    private fun getTokenInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val token = sharedPreferences.getString("token", "") ?: ""
+            val request = chain.request().newBuilder()
+                .addHeader(
+                    "Authorization",
+                    "Bearer $token"
+                )
+                .build()
+            chain.proceed(request)
+        }
+    }
+
+    fun loginApi(): AuthApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(getLoggingInterceptor())
+                    .build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AuthApi::class.java)
+    }
+}
